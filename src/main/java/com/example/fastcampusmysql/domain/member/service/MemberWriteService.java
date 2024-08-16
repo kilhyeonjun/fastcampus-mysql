fd4b7a16fb7dfacd24878d7136124d0c39d924c2
kilhyeonjun <kboxstar@gmail.com>
2024-08-16 16:29:12 +0900

    feat: 회원 정보 등록 구현

---
+++ src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
@@ -0,0 +1,37 @@
+package com.example.fastcampusmysql.domain.member.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.fastcampusmysql.domain.member.dto.RegisterMemberCommand;
+import com.example.fastcampusmysql.domain.member.entity.Member;
+import com.example.fastcampusmysql.domain.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class MemberWriteService {
+    final private MemberRepository memberRepository;
+
+    public Member create(RegisterMemberCommand command) {
+        /*
+         * 목표
+         * - 회원정보(이메일, 닉네임, 생년월일)를 등록한다.
+         * - 닉네임은 10자를 넘길 수 없다.
+         * 
+         * 파라미터
+         * - memberRegisterCommand
+         * 
+         * val member = Member.of(memberRegisterCommand)
+         * memberRepository.save(member);
+         */
+        var member = Member.builder()
+                .email(command.email())
+                .nickname(command.nickname())
+                .birthday(command.birthday())
+                .build();
+
+        return memberRepository.save(member);
+    }
+
+}
